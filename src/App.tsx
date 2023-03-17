8e73877cbc4772616b6d6be938c16bfb09cadbcd
kasunbuddhi <kasunbuddhi@gmail.com>
2023-03-17 19:16:13 +0530

    functional / class component with Route commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, Link} from 'react-router-dom';
 import { MyFunction } from './components/MyFuncComp';
 import { MyClassComp } from './components/MyClassComp';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/test"> Test </Link>
+          </li>
+        </ul>
+      </nav>
      <Routes>
        <Route path='/' element={<MyClassComp/>} />
        <Route path='/test' element={<MyFunction name = "xyz"/>} />
